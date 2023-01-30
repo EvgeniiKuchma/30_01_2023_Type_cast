33a8b9b53808f3d1ab91e92cf3c88002487cf67c
Kuchma <desferto@mail.ru>
2023-01-30 21:12:50 +0500

    Добавьте файлы проекта.

---
+++ Main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+class Car {
+public:
+	void foo() {
+		std::cout << "i,ts Car" << std::endl;
+	}
+};
+class Van : public Car{
+public:
+	void foo() {
+		std::cout << "i,ts Van" << std::endl;
+	}
+};
+int main() {
+	
+	int j;
+	double i = 32200030001.1;
+	j = static_cast<int>(i);//произошло урезание типа double до типа int 
+	//static_cast перед этим провел проверку совместимости типов, но так как диапазон дабл 
+	//выше диапазона инт вывелось последнее значение диапазона инт 
+	std::cout << j << std::endl;
+	
+	std::cout << "********************" << std::endl;
+	double  a = 3.41;
+	const double *pC = &a;
+	double* ppC = const_cast<double*>(pC);
+	std::cout << typeid(* ppC).name() << std::endl;
+
+	std::cout << "********************" << std::endl;
+	//Van* pVan = (Van*)pcar не удачный даун каст до появления обьектов
+	Car car;
+	Van van;
+	Car* pcar = &van;//upcast Van to Car
+	Van* pVan = (Van*)pcar;//downcast Car to Van
+	//Van* pVan = dynamic_cast<Van*>(&pcar) не удачный даун каст в результате не виртуального наследования
+	//downcast UB
+	//Van* pvan = (Van*)&pcar;передача по ссылке обьекта даункаста не возможна
+	return 0;
+};
